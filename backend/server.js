8f074332e51a302a9cf9e9e73b1a0aab6224655e
Ayeshausma07 <uayesha832@gmail.com>
2025-08-02 16:03:17 +0500

    Fixed CORS with env frontend url

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin:  process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
